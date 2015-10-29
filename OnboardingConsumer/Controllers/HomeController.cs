9d6cc1a22f95c057f63c9b4b649f1181e7966b6c
Matt Harding <matt.harding@reyker.com>
2015-10-29 12:19:01 +0000

    Initial commit

---
+++ OnboardingConsumer/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using OnboardingConsumer.Models;
+using OnboardingConsumer.Models.ReykerOnboarding.Models;
+using OnboardingConsumer.Utilities;
+
+namespace OnboardingConsumer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            ClientDetails cd = await PostOnboardingClientDetails();
+            return View(cd);
+        }
+
+        public async Task<ClientDetails> PostOnboardingClientDetails()
+        {
+            var model = new ClientDetails();
+            var submittionData = new OnboardingClientDetails()
+            {
+                Title = "Mr",
+                Forenames = "John",
+                Surname = "Doe",
+                CountryOfBirth = 1,
+                EmailAddress = "john.doe@reyker.com",
+                BirthDate = DateTime.Now.AddYears(-30),
+                PrimaryAddress = new OnboardingPrimaryAddress() { Address1 = "Street 1", City = "London", Postcode = "E15JP", Country = 1},
+                PrimaryTelephone = new OnboardingTelephoneNumber() { DialingCode = 1, Number = "0770345657", TelephoneType = "Mobile"},
+                BankAccount = new OnboardingBankAccount() { AccountName = "John Doe Account", AccountNumber = "123456789", SortCode = "12-34-56"},
+                PrimaryCitizenship = new OnboardingCitizenship() { CountryOfResidency = 1, TaxIdentificationNumber = "AZ34654Z"},
+                PlanType = 1,
+                ExternalCustomerId = "209",
+                ExternalPlanId = "1"
+            };
+            
+
+
+            using (new HttpClient())
+            {
+                HttpWebRequest request = WebRequest.CreateHttp("http://reykeronboarding.azurewebsites.net/api/EncryptedValues");
+                request.ContentType = "text/json";
+                request.Method = "POST";
+
+                //NEED TO CHANGE USERNAME TO PROVIDED USERNAME
+                var authHeader = "Reyker USERNAME";
+                request.Headers.Add("Authorization", authHeader);
+
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {
+                    string encryptedSubmissionData = await submittionData.AES_Encrypt();
+                    string json = new JavaScriptSerializer().Serialize(encryptedSubmissionData);
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+
+                using (var response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response != null && response.StatusCode == HttpStatusCode.OK)
+                    {
+                        using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                        {
+                            string objText = reader.ReadToEnd();
+                            model = await objText.AES_Decrypt<ClientDetails>();
+                        }
+                    }
+                }
+            }
+            return model;
+        }
+    }
+}
---
+++ OnboardingConsumer/Models/ClientDetails.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnboardingConsumer.Models
+{
+    public class ClientDetails
+    {
+        //Identification
+        public int ReykerId { get; set; }
+        public string ExternalId { get; set; }
+
+        //Personal Details
+
+        public string Title { get; set; }
+        public string Forenames { get; set; }
+        public string Surname { get; set; }
+        public string CountryOfBirth { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string Suffix { get; set; }
+        public string NationalInsuranceNumber { get; set; }
+
+        //Telephone Numbers
+
+        public List<TelephoneNumber> TelephoneNumbers { get; set; }
+
+        //Addresses 
+
+        public List<ClientAddress> Addresses { get; set; }
+
+        //Bank account details
+
+        public ClientBankAccount BankAccount { get; set; }
+
+        //Citizenship Details
+
+        public List<ClientCitizenship> Citizenships { get; set; }
+
+        //AML
+        public ClientAML AML { get; set; }
+
+        //Plan
+        public List<ClientPlan> Plans { get; set; }
+
+        //Error
+        public string ErrorMessage { get; set; }
+    }
+
+    public class ClientPlan
+    {
+        public string Provider { get; set; }
+        public string PlanType { get; set; }
+        public int PlanTypeId { get; set; }
+        public string PlanName { get; set; }
+        public string ExternalPlanId { get; set; }
+        public string Promotion { get; set; }
+        public List<ClientCashTransaction> CashTransactions { get; set; }
+        public List<ClientSecurityTransaction> SecurityTransactions { get; set; }
+    }
+
+    public class ClientCashTransaction
+    {
+        public string TransactionDescription { get; set; }
+        public double? TransactionAmount { get; set; }
+        public DateTime? TransactionDate { get; set; }
+    }
+
+    public class ClientSecurityTransaction
+    {
+        public string TransactionDescription { get; set; }
+        public double? TransactionAmount { get; set; }
+        public DateTime? TransactionDate { get; set; }
+    }
+
+    public class TelephoneNumber
+    {
+        public string Number { get; set; }
+
+        public string DialingCode { get; set; }
+
+        public string TelephoneType { get; set; }
+
+        public int TelephoneTypeId { get; set; }
+    }
+
+    public class ClientAddress
+    {
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+
+        public string County { get; set; }
+
+        public string Postcode { get; set; }
+
+        public string Country { get; set; }
+
+        public int CountryId { get; set; }
+
+        public string AddressType { get; set; }
+
+        public int AddressTypeId { get; set; }
+    }
+
+    public class ClientCitizenship
+    {
+        public string CountryOfResidency { get; set; }
+        public int CountryOfResidencyId { get; set; }
+        public string TaxIdentificationNumber { get; set; }
+    }
+
+    public class ClientBankAccount
+    {
+        public string AccountName { get; set; }
+        public string AccountNumber { get; set; }
+        public string SortCode { get; set; }
+    }
+
+    public class ClientAML
+    {
+        public string AMLStatus { get; set; }
+        public DateTime? AMLDate { get; set; }
+        public string AMLScore { get; set; }
+    }
+
+    public class PlanTypeDetails
+    {
+        public int PlanTypeId { get; set; }
+        public string PlanType { get; set; }
+    }
+
+    public class CountryDetails
+    {
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+
+    public class DialingCodeDetails
+    {
+        public int DialingCodeId { get; set; }
+        public string Location { get; set; }
+        public string LocationFull { get; set; }
+        public string IDC { get; set; }
+    }
+
+}
