a11843ac2aa10538a69afc8bc909282020dbe0cd
Matt Harding <matt.harding@reyker.com>
2016-08-08 17:23:28 +0100

    Added user form

--- OnboardingConsumer/Controllers/HomeController.cs
+++ OnboardingConsumer/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
@@ -8,96 +7,24 @@ using System.Threading.Tasks;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
 using OnboardingConsumer.Models;
-using OnboardingConsumer.Models.ReykerOnboarding.Models;
 using OnboardingConsumer.Utilities;
 
 namespace OnboardingConsumer.Controllers
 {
     public class HomeController : Controller
     {
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
-            //var model = PostOnboardingClientDetails();
-
-            //var model = GetClientDetails();
-
-            //var model = GetAllClientsDetails();
-
-            //var model = AddAdditionalPlan();
-
             return View();
         }
 
-        public async Task<ClientDetails> PostOnboardingClientDetails()
+        [HttpPost]
+        public async Task<ActionResult> PostOnboardingClientDetails(OnboardingClientDetails submitionData)
         {
             var model = new ClientDetails();
-            var submitionData = new OnboardingClientDetails()
-            {
-                Title = "Mr",
-                Forenames = "John",
-                Surname = "Doe",
-                CountryOfBirth = 1,
-                EmailAddress = "john.doe1989@reyker.com",
-                EmailType = "WORK",
-                BirthDate = DateTime.Now.AddYears(-30),
-                PrimaryAddress = new OnboardingPrimaryAddress() { Address1 = "Street 1", City = "London", Postcode = "E15JP", Country = 1},
-                PrimaryTelephone = new OnboardingTelephoneNumber() { DialingCode = 1, Number = "0770345657", TelephoneType = 1},
-                BankAccount = new OnboardingBankAccount() { AccountName = "John Doe Account", AccountNumber = "123456789", SortCode = "12-34-56"},
-                PrimaryCitizenship = new OnboardingCitizenship() { CountryOfResidency = 1, TaxIdentificationNumber = "AZ34654Z"},
-                PlanType = 10,
-                ExternalCustomerId = "ExternalCustomerId",
-                ExternalPlanId = "ExternalPlanId"
-            };
-            
             using (new HttpClient())
             {
-                    var request = WebRequest.CreateHttp("https://reykeronboardingexternaltest.azurewebsites.net/api/Onboarding/");
-                    request.ContentType = "text/json";
-                    request.Method = "POST";
-
-                    //NEED TO CHANGE USERNAME TO PROVIDED USERNAME
-                    const string authHeader = "Reyker USERNAME";
-                    request.Headers.Add("Authorization", authHeader);
-
-                    using (var streamWriter = new StreamWriter(request.GetRequestStream()))
-                    {
-                        var encryptedSubmissionData = await submitionData.AES_Encrypt();
-                        var json = new JavaScriptSerializer().Serialize(encryptedSubmissionData);
-                        streamWriter.Write(json);
-                        streamWriter.Flush();
-                        streamWriter.Close();
-                    }
-
-                    using (var response = request.GetResponse() as HttpWebResponse)
-                    {
-                        var x = response;
-                        if (response != null && response.StatusCode == HttpStatusCode.OK)
-                        {
-                            using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
-                            {
-                                var objText = reader.ReadToEnd();
-                                model = await objText.AES_Decrypt<ClientDetails>();
-                            }
-                        }
-                    }
-            }
-            return model;
-        }
-
-        public async Task<ClientDetails> AddAdditionalPlan()
-        {
-            var model = new ClientDetails();
-
-            var submitionData = new OnboardingPlanDetails()
-            {
-                 ExternalCustomerId = "ExternalCustomerId",
-                 ExternalPlanId = "ExternalPlanId",
-                 PlanType = 10
-            };
-
-            using (new HttpClient())
-            {
-                var request = WebRequest.CreateHttp("https://reykeronboardingexternaltest.azurewebsites.net/api/AdditionalPlan/");
+                var request = WebRequest.CreateHttp("https://reykeronboardingexternaltest.azurewebsites.net/api/Onboarding/");
                 request.ContentType = "text/json";
                 request.Method = "POST";
 
@@ -114,9 +41,10 @@ namespace OnboardingConsumer.Controllers
                     streamWriter.Close();
                 }
 
-                using (var response = request.GetResponse() as HttpWebResponse)
+                try
                 {
-                    var x = response;
+                    var response = request.GetResponse() as HttpWebResponse;
+
                     if (response != null && response.StatusCode == HttpStatusCode.OK)
                     {
                         using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
@@ -125,83 +53,147 @@ namespace OnboardingConsumer.Controllers
                             model = await objText.AES_Decrypt<ClientDetails>();
                         }
                     }
+                    ViewData["ResponseStatusCode"] = response.StatusCode;
+                    ViewData["ResponseStatusMessage"] = response.StatusDescription;
                 }
-            }
-            return model;
-        }
-
-        public async Task<ClientDetails> GetClientDetails()
-        {
-            var model = new ClientDetails();
-
-            using (new HttpClient())
-            {
-                var request = WebRequest.CreateHttp("https://reykeronboardingexternaltest.azurewebsites.net/api/ClientDetails/48707");
-                request.ContentType = "text/json";
-                request.Method = "GET";
-
-                var authHeader = "Reyker USERNAME";
-                request.Headers.Add("Authorization", authHeader);
-
-                try
+                catch (WebException ex)
                 {
+                    var response = ex.Response as HttpWebResponse;
 
-                    using (var response = request.GetResponse() as HttpWebResponse)
+                    using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
                     {
-                        var x = 1;
-                        if (response != null && response.StatusCode == HttpStatusCode.OK)
+                        var objText = reader.ReadToEnd();
+                        if (!string.IsNullOrEmpty(objText))
                         {
-                            using (var reader = new StreamReader(response.GetResponseStream()))
-                            {
-                                var objText = await reader.ReadToEndAsync();
-                                model = await objText.AES_Decrypt<ClientDetails>();
-                            }
+                            model = await objText.AES_Decrypt<ClientDetails>();
                         }
                     }
-                }
-                catch (Exception ex)
-                {
-                    model.ErrorMessage = ex.Message;
-                }
-            }
-            return model;
-        }
 
-        public async Task<List<ClientDetails>> GetAllClientsDetails()
-        {
-            var model = new List<ClientDetails>();
-
-            using (new HttpClient())
-            {
-                var request = WebRequest.CreateHttp("https://reykeronboardingexternaltest.azurewebsites.net/api/BulkClientDetails/");
-                request.ContentType = "text/json";
-                request.Method = "GET";
-
-                var authHeader = "Reyker USERNAME";
-                request.Headers.Add("Authorization", authHeader);
-
-                try
-                {
-
-                    using (var response = request.GetResponse() as HttpWebResponse)
-                    {
-                        var x = 1;
-                        if (response != null && response.StatusCode == HttpStatusCode.OK)
-                        {
-                            using (var reader = new StreamReader(response.GetResponseStream()))
-                            {
-                                var objText = await reader.ReadToEndAsync();
-                                model = await objText.AES_Decrypt<List<ClientDetails>>();
-                            }
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    var error = ex.Message;
+                    ViewData["ResponseStatusCode"] = response.StatusCode;
+                    ViewData["ResponseStatusMessage"] = response.StatusDescription;
                 }
             }
-            return model;
+            return View("OnboardingResult", model);
         }
+
+        //public async Task<ClientDetails> AddAdditionalPlan()
+        //{
+        //    var model = new ClientDetails();
+
+        //    var submitionData = new OnboardingPlanDetails()
+        //    {
+        //         ExternalCustomerId = "ExternalCustomerId",
+        //         ExternalPlanId = "ExternalPlanId",
+        //         PlanType = 10
+        //    };
+
+        //    using (new HttpClient())
+        //    {
+        //        var request = WebRequest.CreateHttp("https://reykeronboardingexternaltest.azurewebsites.net/api/AdditionalPlan/");
+        //        request.ContentType = "text/json";
+        //        request.Method = "POST";
+
+        //        //NEED TO CHANGE USERNAME TO PROVIDED USERNAME
+        //        const string authHeader = "Reyker USERNAME";
+        //        request.Headers.Add("Authorization", authHeader);
+
+        //        using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+        //        {
+        //            var encryptedSubmissionData = await submitionData.AES_Encrypt();
+        //            var json = new JavaScriptSerializer().Serialize(encryptedSubmissionData);
+        //            streamWriter.Write(json);
+        //            streamWriter.Flush();
+        //            streamWriter.Close();
+        //        }
+
+        //        using (var response = request.GetResponse() as HttpWebResponse)
+        //        {
+        //            var x = response;
+        //            if (response != null && response.StatusCode == HttpStatusCode.OK)
+        //            {
+        //                using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+        //                {
+        //                    var objText = reader.ReadToEnd();
+        //                    model = await objText.AES_Decrypt<ClientDetails>();
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return model;
+        //}
+
+        //public async Task<ClientDetails> GetClientDetails()
+        //{
+        //    var model = new ClientDetails();
+
+        //    using (new HttpClient())
+        //    {
+        //        var request = WebRequest.CreateHttp("https://reykeronboardingexternaltest.azurewebsites.net/api/ClientDetails/48707");
+        //        request.ContentType = "text/json";
+        //        request.Method = "GET";
+
+        //        var authHeader = "Reyker USERNAME";
+        //        request.Headers.Add("Authorization", authHeader);
+
+        //        try
+        //        {
+
+        //            using (var response = request.GetResponse() as HttpWebResponse)
+        //            {
+        //                var x = 1;
+        //                if (response != null && response.StatusCode == HttpStatusCode.OK)
+        //                {
+        //                    using (var reader = new StreamReader(response.GetResponseStream()))
+        //                    {
+        //                        var objText = await reader.ReadToEndAsync();
+        //                        model = await objText.AES_Decrypt<ClientDetails>();
+        //                    }
+        //                }
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            model.ErrorMessage = ex.Message;
+        //        }
+        //    }
+        //    return model;
+        //}
+
+        //public async Task<List<ClientDetails>> GetAllClientsDetails()
+        //{
+        //    var model = new List<ClientDetails>();
+
+        //    using (new HttpClient())
+        //    {
+        //        var request = WebRequest.CreateHttp("https://reykeronboardingexternaltest.azurewebsites.net/api/BulkClientDetails/");
+        //        request.ContentType = "text/json";
+        //        request.Method = "GET";
+
+        //        var authHeader = "Reyker USERNAME";
+        //        request.Headers.Add("Authorization", authHeader);
+
+        //        try
+        //        {
+
+        //            using (var response = request.GetResponse() as HttpWebResponse)
+        //            {
+        //                var x = 1;
+        //                if (response != null && response.StatusCode == HttpStatusCode.OK)
+        //                {
+        //                    using (var reader = new StreamReader(response.GetResponseStream()))
+        //                    {
+        //                        var objText = await reader.ReadToEndAsync();
+        //                        model = await objText.AES_Decrypt<List<ClientDetails>>();
+        //                    }
+        //                }
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            var error = ex.Message;
+        //        }
+        //    }
+        //    return model;
+        //}
     }
 }
--- OnboardingConsumer/Models/OnboardingClientDetails.cs
+++ OnboardingConsumer/Models/OnboardingClientDetails.cs
@@ -1,129 +1,125 @@
-﻿namespace OnboardingConsumer.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+namespace OnboardingConsumer.Models
 {
-    using System;
-    using System.ComponentModel.DataAnnotations;
-    using System.Collections.Generic;
-
-    namespace ReykerOnboarding.Models
+    [Serializable]
+    public class OnboardingClientDetails
     {
-        [Serializable]
-        public class OnboardingClientDetails
-        {
-            //Personal Details
-            [Required]
-            public string Title { get; set; }
-            [Required]
-            public string Forenames { get; set; }
-            [Required]
-            public string Surname { get; set; }
-            [Required]
-            public int CountryOfBirth { get; set; }
-            [Required]
-            public string EmailAddress { get; set; }
-            [Required]
-            public string EmailType { get; set; }
-            [Required]
-            public DateTime BirthDate { get; set; }
+        //Personal Details
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Forenames { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public int CountryOfBirth { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string EmailType { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
 
-            //Additional Personal Details
-            public string Suffix { get; set; }
-            public string NationalInsuranceNumber { get; set; }
+        //Additional Personal Details
+        public string Suffix { get; set; }
+        public string NationalInsuranceNumber { get; set; }
 
-            // Primary Address
-            [Required]
-            public OnboardingPrimaryAddress PrimaryAddress { get; set; }
+        // Primary Address
+        [Required]
+        public OnboardingPrimaryAddress PrimaryAddress { get; set; }
 
-            //Additional Addresses 
-            public List<OnboardingAddress> AdditionalAddresses { get; set; }
+        //Additional Addresses 
+        public List<OnboardingAddress> AdditionalAddresses { get; set; }
 
-            //Primary Telephone
-            [Required]
-            public OnboardingTelephoneNumber PrimaryTelephone { get; set; }
+        //Primary Telephone
+        [Required]
+        public OnboardingTelephoneNumber PrimaryTelephone { get; set; }
 
-            //Additional Telephone
-            public List<OnboardingTelephoneNumber> AdditionalTelephone { get; set; }
+        //Additional Telephone
+        public List<OnboardingTelephoneNumber> AdditionalTelephone { get; set; }
 
-            //Bank account details
-            [Required]
-            public OnboardingBankAccount BankAccount { get; set; }
+        //Bank account details
+        [Required]
+        public OnboardingBankAccount BankAccount { get; set; }
 
-            //Citizenship Details
-            [Required]
-            public OnboardingCitizenship PrimaryCitizenship { get; set; }
+        //Citizenship Details
+        [Required]
+        public OnboardingCitizenship PrimaryCitizenship { get; set; }
 
-            public List<OnboardingCitizenship> AdditionalCitizenship { get; set; }
+        public List<OnboardingCitizenship> AdditionalCitizenship { get; set; }
 
-            //External Provider Info
-            [Required]
-            public string ExternalCustomerId { get; set; }
-            [Required]
-            public string ExternalPlanId { get; set; }
+        //External Provider Info
+        [Required]
+        public string ExternalCustomerId { get; set; }
+        [Required]
+        public string ExternalPlanId { get; set; }
 
-            [Required]
-            public int PlanType { get; set; }
-
-        }
-
-        public class OnboardingAddress
-        {
-            [Required]
-            public string Address1 { get; set; }
-
-            public string Address2 { get; set; }
-            [Required]
-            public string City { get; set; }
-
-            public string County { get; set; }
-            [Required]
-            public string Postcode { get; set; }
-            [Required]
-            public int Country { get; set; }
-            [Required]
-            public int AddressType { get; set; }
-        }
-
-        public class OnboardingPrimaryAddress
-        {
-            [Required]
-            public string Address1 { get; set; }
-
-            public string Address2 { get; set; }
-            [Required]
-            public string City { get; set; }
-
-            public string County { get; set; }
-            [Required]
-            public string Postcode { get; set; }
-            [Required]
-            public int Country { get; set; }
-        }
-
-        public class OnboardingTelephoneNumber
-        {
-            [Required]
-            public string Number { get; set; }
-            [Required]
-            public int DialingCode { get; set; }
-            [Required]
-            public int TelephoneType { get; set; }
-        }
-
-        public class OnboardingBankAccount
-        {
-            [Required]
-            public string AccountName { get; set; }
-            [Required]
-            public string AccountNumber { get; set; }
-            [Required]
-            public string SortCode { get; set; }
-        }
-
-        public class OnboardingCitizenship
-        {
-            public int CountryOfResidency { get; set; }
-            public string TaxIdentificationNumber { get; set; }
-        }
+        [Required]
+        public int PlanType { get; set; }
 
     }
 
+    public class OnboardingAddress
+    {
+        [Required]
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+        [Required]
+        public string City { get; set; }
+
+        public string County { get; set; }
+        [Required]
+        public string Postcode { get; set; }
+        [Required]
+        public int Country { get; set; }
+        [Required]
+        public int AddressType { get; set; }
+    }
+
+    public class OnboardingPrimaryAddress
+    {
+        [Required]
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+        [Required]
+        public string City { get; set; }
+
+        public string County { get; set; }
+        [Required]
+        public string Postcode { get; set; }
+        [Required]
+        public int Country { get; set; }
+    }
+
+    public class OnboardingTelephoneNumber
+    {
+        [Required]
+        public string Number { get; set; }
+        [Required]
+        public int DialingCode { get; set; }
+        [Required]
+        public int TelephoneType { get; set; }
+    }
+
+    public class OnboardingBankAccount
+    {
+        [Required]
+        public string AccountName { get; set; }
+        [Required]
+        public string AccountNumber { get; set; }
+        [Required]
+        public string SortCode { get; set; }
+    }
+
+    public class OnboardingCitizenship
+    {
+        public int CountryOfResidency { get; set; }
+        public string TaxIdentificationNumber { get; set; }
+    }
+
 }
+
